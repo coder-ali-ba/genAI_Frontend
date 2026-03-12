876cb5e9497b93ed059928f83bda175edf1db3df
coder-ali-ba <habibali8770@gmail.com>
2026-03-12 23:12:16 +0500

    authentications

---
+++ src/features/auth/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main>
+        <div className='form-container'>
+            <h1>Login</h1>
+            <form>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name='email' id="email" placeholder='Enter email address'/>
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' id="password" placeholder='Enter password'/>
+                </div>
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
