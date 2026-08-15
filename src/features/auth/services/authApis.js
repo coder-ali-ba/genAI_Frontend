a0fa881a85ca8a21462b3b35320dd1961efa3db0
coder-ali-ba <habibali8770@gmail.com>
2026-08-15 14:58:36 +0500

    hshshs

--- src/features/auth/services/authApis.js
+++ src/features/auth/services/authApis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL : "http://gen-ai-ebon-omega.vercel.app",
+  baseURL : "https://gen-ai-ebon-omega.vercel.app",
   withCredentials : true
 })
 
